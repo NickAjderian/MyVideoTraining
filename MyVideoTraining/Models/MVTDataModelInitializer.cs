25ef9299a37fd784af7d2a234470db76013627ca
Nick Ajderian <nickajderian@hotmail.com>
2015-05-01 17:20:43 +0100

    sorting non required local files

---
+++ MyVideoTraining/Models/MVTDataModelInitializer.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MyVideoTraining.Models
+{
+    public class MVTDataModelInitializer:System.Data.Entity.MigrateDatabaseToLatestVersion<MVTDataModel,MyVideoTraining.Migrations.Configuration>
+    {
+        
+    }
+}
