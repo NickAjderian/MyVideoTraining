1122c60c7452a511af125713a9aa052b0cacc204
Nick Ajderian <nickajderian@hotmail.com>
2015-05-07 23:40:27 +0100

    cached candidates

--- README.md
+++ README.md
@@ -15,3 +15,14 @@ start CNTLM, a proxy server. Shut down service after use.
 
 http://mobileangularui.com/blog/your-first-phonegap-app-with-mobile-angular-ui/
 
+On Phonegap Build
+https://build.phonegap.com/apps/1392453/builds
+
+On the Google Play Developer Console:
+https://play.google.com/apps/publish/?dev_acc=08964237641650064751#AppListPlace
+
+On the Survitech community
+https://plus.google.com/communities/116780627671674897925
+
+On the Google Play store
+https://play.google.com/apps/testing/net.flikk.myvideotraining
--- platforms/android/assets/www/js/index.js
+++ platforms/android/assets/www/js/index.js
@@ -66,6 +66,8 @@ var app = {
 
 app.initialize();
 
+var APIURL = (document.URL.match("^file") ? 'https://myvideotraining.azurewebsites.net/' : '/') + 'api/';
+
 var wrapper = document.getElementById("signature-pad"),
     clearButton = wrapper.querySelector("[data-action=clear]"),
     saveButton = wrapper.querySelector("[data-action=save]"),
@@ -87,13 +89,82 @@ function resizeCanvas() {
 window.onresize = resizeCanvas;
 resizeCanvas();
 
+var getPointerEvent = function (event) {
+    return event.originalEvent.targetTouches ? event.originalEvent.targetTouches[0] : event;
+};
+
 angular.module('app', []);
-angular.module('app').controller('mainCtrl', function ($scope, $http) {
+angular.module('app').controller('mainCtrl', function ($scope, $http, assignmentSvc) {
     $scope.status = 'ok so far';
     $scope.state = 'capturelogin';
     $scope.videoHasEnded = true;
+    $scope.healthControl = $("#HealthSlider");
+    $scope.healthValue = 50;
+    $scope.healthIndicator = 50;
+    $scope.currentSession = {};
+    $scope.sessions = [];
 
-    $scope.run = function(){
+    $scope.loadSessions = function () {
+        if (typeof (Storage) != "undefined") {
+            var ss = JSON.parse(localStorage.getItem("sessions"));
+            if (ss) {
+                $scope.sessions = ss;
+            }
+        }
+    }
+    $scope.saveSessions = function () {
+        if (typeof (Storage) != "undefined") {
+            localStorage.setItem("sessions", JSON.stringify($scope.sessions));
+        }
+        if($scope.sessions){
+       		$scope.sessions.forEach( function(session){
+       			if(!session.saved){
+					assignmentSvc.post(session).then(
+						function(response){
+							if(response === 'OK'){
+								session.saved = true;
+								if (typeof (Storage) != "undefined") {
+									localStorage.setItem("sessions", JSON.stringify($scope.sessions));
+								}								
+							}
+						}
+					);       			       				
+       			}
+       		});
+		}
+
+    }
+    $scope.postSessions = function () {
+
+    }
+	
+	$scope.saveCands = function(){
+        if (typeof (Storage) != "undefined") {
+            localStorage.setItem("candidates", JSON.stringify($scope.cands));
+        }
+	};
+	$scope.loadCands = function(){
+        if (typeof (Storage) != "undefined") {
+            var cc = JSON.parse(localStorage.getItem("candidates"));
+			if(cc){
+				$scope.cands = cc;
+				$scope.loadSessions();
+				$scope.removeAgreedPatients();
+			}
+        }
+	};
+	
+    $scope.onClearLocalData = function(){
+    	$scope.sessions = [];
+    	$scope.saveSessions();
+    }
+
+
+    $scope.run = function () {
+		$scope.loadCands();
+        $("#HealthSlider").on('touchstart mousedown mouseup mousemove', $scope.healthTap);
+        $("#HealthIndicator").on('touchstart mousedown mouseup mousemove', $scope.healthTap);
+        $scope.state = 'capturelogin';
     };
 
 
@@ -103,12 +174,53 @@ angular.module('app').controller('mainCtrl', function ($scope, $http) {
         $scope.getPatients();
     };
     $scope.getPatients = function () {
-        $http.get(APIURL + "candidates").then(function (data) {
-            $scope.cands = JSON.parse(data.data).Result;
+        $http.get(APIURL + "assignmentstodo").then(function (data) {
+            $scope.cands = JSON.parse(data.data);
             //cands = [{ClientID, PersonName,Assignments:[{AssignmentID, AssignmentType { AssignmenttypeName}}]}]
-        });
+        	$scope.loadSessions();
+            $scope.removeAgreedPatients();
+			$scope.saveCands();
+        }, function(data,error){
+			alert(JSON.stringify(data));
+		});
     };
-    $scope.onSelectPerson = function(person){
+
+    $scope.removeAgreedPatients = function(){
+        //remove any patients and their assignments if they have agreed
+        var completedAssignments = [];
+        var patientIndex = 0;
+        var assignmentIndex = 0;
+        var patient;
+        if($scope.sessions){
+			$scope.sessions.forEach(function (session) {
+				if (session.response === "agreed") {
+					completedAssignments.push(session.assignmentId);
+					for (patientIndex = $scope.cands.length - 1; patientIndex > -1;patientIndex--)
+					{
+						patient = $scope.cands[patientIndex];
+						for(assignmentIndex = patient.Assignments.length-1; assignmentIndex > -1; --assignmentIndex){
+							if(patient.Assignments[assignmentIndex].AssignmentId === session.assignmentId){
+								patient.Assignments.splice(assignmentIndex,1);
+							}
+						}
+						if (patient.Assignments.length === 0) {
+							$scope.cands.splice(patientIndex, 1);
+						}
+					}
+				}
+			});
+        	
+        }
+
+
+    }
+
+    $scope.onSelectPerson = function (person) {
+        $scope.currentSession = {
+            userEmail: 'nick@flikk.net',
+            personId: person.PersonId,
+            personName: person.PersonName
+        };
         $scope.selectedPerson = person;
     }
     $scope.onChoosePatient = function () {
@@ -118,18 +230,26 @@ angular.module('app').controller('mainCtrl', function ($scope, $http) {
     $scope.vid = document.getElementById('videoControl');
     $scope.onChooseAssignment = function (assignment) {
         $scope.selectedAssignment = assignment;
-        if (assignment.AssignmentType.Medias.length == 1) {
-            $scope.onChooseMedia(assignment.AssignmentType.Medias[0]);
+        $scope.currentSession.assignmentId = assignment.AssignmentId;
+        var itemToAdd = $scope.currentSession;
+        $scope.sessions.forEach(function (ss) {
+            if (ss === itemToAdd) {
+                //its there dont need to add
+                itemToAdd = null;
+            }
+        });
+        if (itemToAdd) {
+            $scope.sessions.push(itemToAdd);
         }
-    };
-    $scope.onChooseMedia = function (media) {
         $scope.state = 'watchvideo'
         //TODO: put assignment.Medias[0].Url into video player
-        $scope.vid.src = media.Url;
+        $scope.vid.src = assignment.AssignmentType.MediaUrl;
         $scope.vid.load();
-    }
+    };
 
     $scope.vid.onended = function () {
+        $scope.currentSession.videoHasEnded = true;
+        $scope.currentSession.videoWatched= new Date().valueOf();
         $scope.videoHasEnded = true;
         $scope.$apply();
     };
@@ -143,10 +263,12 @@ angular.module('app').controller('mainCtrl', function ($scope, $http) {
     };
     $scope.onReject = function () {
         //TODO: log reject response
+        $scope.currentSession.response = "not agreed";
         $scope.state = 'capturethanks';
     };
 	$scope.onAgree = function(){
-		$scope.state='capturesignature';
+
+	    $scope.state = 'capturesignature';
 		if(!$scope.signaturePad){
             $scope.signaturePad = new SignaturePad(canvas);
 		}
@@ -155,9 +277,66 @@ angular.module('app').controller('mainCtrl', function ($scope, $http) {
         }
 	};
 	$scope.onSigned = function(){
-		$scope.signatureDataUrl = $scope.signaturePad.toDataURL();
+	    $scope.currentSession.signatureDataUrl = $scope.signaturePad.toDataURL();
 		$scope.state='capturehealth';
+        $scope.healthValue = 50;
+        setTimeout($scope.setHealthIndicator,500);
 	};
+	
+	$scope.healthTap = function (e) {
+	    e.preventDefault();
+	    var pointer = getPointerEvent(e);
+	    var target = document.getElementById("HealthSlider").getBoundingClientRect();
+	    var indicator = document.getElementById("HealthIndicator").getBoundingClientRect();
+	    $scope.healthValue = Math.floor(100 * (pointer.clientX - target.left) / target.width);
+	    if ($scope.healthValue < 0) {
+	        $scope.healthValue = 0;
+	    } else if ($scope.healthValue > 100) {
+	        $scope.healthValue = 100;
+	    }
+	    $scope.setHealthIndicator();
+	};
+	
+	$scope.setHealthIndicator = function(){
+		//position indicator in the correct position for healthValue
+
+	    var target = document.getElementById("HealthSlider").getBoundingClientRect();
+	    var indicator = document.getElementById("HealthIndicator").getBoundingClientRect();
+
+	    $scope.healthIndicator = $scope.healthValue * target.width / 100  + target.left - indicator.width / 2;
+	    $scope.$apply();
+	}
+
+
+	$scope.onHealthCompleted = function () {
+	    $scope.currentSession.response = "agreed";
+	    $scope.currentSession.healthValue = $scope.healthValue;
+	    $scope.currentSession.completed = new Date().toISOString();
+	    $scope.currentSession = null;
+	    $scope.saveSessions();
+	    $scope.removeAgreedPatients();
+	    $scope.state = 'capturethanks';
+	}
+
     $scope.run();
 
 });
+angular.module('app').factory('assignmentSvc',['$q','$http', function($q,$http){
+    return {
+        post: function (assignment) {
+            var deferred = $q.defer();
+            $http.post(APIURL + 'assignment',assignment)
+            .success(function(data,status){
+            	deferred.resolve('OK');
+            })
+            .error(function(data, status){
+            	var msg = 'Could not post';
+            	if(data.ExceptionMessage){
+            		msg = data.ExceptionMessage;
+            	}
+            	deferred.resolve(msg);
+            });
+            return deferred.promise;
+        }
+    };
+}]);
